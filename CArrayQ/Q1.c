5164020b78146100324bfe2840b6b3521cfa231d
oringejooz <bishtswarnima@gmail.com>
2024-06-07 17:03:18 +0530

    Changes to be committed: 	modified:   .vscode/launch.json 	modified:   CArrayQ/Q1.c 	modified:   CArrayQ/Q1.exe

--- CArrayQ/Q1.c
+++ CArrayQ/Q1.c
@@ -11,11 +11,12 @@ input 10 elements in the array*/
 
 int main(){
     int n;
+    
     printf("Enter number of elements in the array ");
     scanf("%d",&n);
     int arr[n];
     for(int i=0;i<n;i++){
-        printf("Enter the element at position %d \n",i);
+        printf("Enter the element at position %d \n",i+1);
         scanf("%d",&arr[i]);
     }
     for(int i=0;i<n;i++){
